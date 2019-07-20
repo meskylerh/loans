a3fe0e6e51a8cf282711608ca6fdb1121a99f38b
Ryan Hildreth <meryanh@gmail.com>
2019-07-20 13:56:09 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// tell it to use the public directory as one where static files live
+app.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => {
+		const params = {error: undefined};
+		res.render('signin', params);
+	})
+	.post('/login', getLogin)
+	.post('/create_account', getsignup)
+	.get('/signup', (req, res) => {
+		const params = {error: null};
+		res.render('signup', params);
+	})
+	.listen(port, function() {
+		console.log('Node app is running on port', port);
+	});
+
+
+function getLogin(req, res) {
+	let username = req.query.username;
+	let password = req.query.password;  
+
+	const params = {username};
+	res.redirect('home.html');
+}
+
+function getsignup(req, res) {
+	let username = req.query.username;
+	let password = req.query.password;  
+
+	const params = {error: 'Oops! You just made an account. This isn\'t actually an error.', username: 'Some Name'};
+	res.render('signin', params);
+}
